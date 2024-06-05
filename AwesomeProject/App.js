01b30357cd5b5a668254860bb2c189a8f2211751
Aleksey Perfilov <aleksey.perfilov03@gmail.com>
2024-06-05 22:22:53 +0300

    Навигапционное меню

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+
+const Tab = createBottomTabNavigator(); // Создание навигационного меню
+
+function Screen1() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Новости</Text>
+    </View>
+  );
+} // Функция создания одного из экранов меню и его содержимого
+
+function Screen2() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Главная</Text>
+    </View>
+  );
+} // Функция создания одного из экранов меню и его содержимого. Здесь можно полностью настроить содержимое страницы, а можно привязать отдельный файл, в котором будут все настройки страницы
+
+function Screen3() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Screen 3</Text>
+    </View>
+  );
+} // Функция создания одного из экранов меню и его содержимого
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Новости" component={Screen1} />
+        <Tab.Screen name="Главная" component={Screen2} />
+        <Tab.Screen name="Мое" component={Screen3} /> 
+      </Tab.Navigator>
+    </NavigationContainer>
+  ); // Отображение приложения. Компонент отвечает за привязку определенной функции стртаницы для отображения в приложении. 
+  // В name можно выбрать название экрана, оно будет отображаться снизу в меню 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
